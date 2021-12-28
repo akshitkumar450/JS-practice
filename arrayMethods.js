12c47f87770030f3b1c512a5a7fa2e16c590c932
Akshit kumar <akshitkumar450@gmail.com>
2021-12-29 01:00:48 +0530

    slice,splice and basic array methods

---
+++ arrayMethods.js
@@ -0,0 +1,53 @@
+/////////////////////////////////////////////////
+// arrays are object and they get access to built in methods
+
+// let arr = ['a', 'b', 'c', 'd', 'e'];
+
+// SLICE
+// it does not change the array
+// slice method return new array
+// console.log(arr.slice(2));
+// console.log(arr.slice(2, 4));
+// console.log(arr.slice(-2)); //last 2
+// console.log(arr.slice(-1)); //last 1
+// console.log(arr.slice(1, -2));
+// console.log(arr.slice()); //shallow copy
+// console.log([...arr]); //shallow copy
+
+// SPLICE (removing elements from array)
+// it changes the given array
+// console.log(arr.splice(2));
+// first is index and second is no of elements to be deleted
+// arr.splice(-1);
+// console.log(arr);
+// arr.splice(1, 2);
+// console.log(arr);
+
+// REVERSE (changes the array)
+// arr = ['a', 'b', 'c', 'd', 'e'];
+// const arr2 = ['j', 'i', 'h', 'g', 'f'];
+// console.log(arr2.reverse());
+// console.log(arr2);
+
+// // CONCAT (does not change the array)
+// const letters = arr.concat(arr2);
+// console.log(arr);
+// console.log(letters);
+// console.log([...arr, ...arr2]);
+
+// // JOIN (join the elements of array by a separator)
+// console.log(letters.join(' * '));
+
+///////////////////////////////////////////
+// The new at Method
+// const arr = [23, 11, 64];
+// console.log(arr[0]);
+// console.log(arr.at(0));
+
+// // getting last array element
+// console.log(arr[arr.length - 1]);
+// console.log(arr.slice(-1)[0]); //returns a new array and then we get the value of that index
+// console.log(arr.at(-1));
+
+// console.log('jonas'.at(0));
+// console.log('jonas'.at(-1));
