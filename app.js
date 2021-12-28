78652ebdcb560721abe988377d481525444b9c8b
Akshit kumar <akshitkumar450@gmail.com>
2021-12-29 00:26:07 +0530

    function accepting callbacks and pass by value and reference

--- app.js
+++ app.js
@@ -376,3 +376,67 @@ const greeterHey = greet('Hey');
 greeterHey('Jonas');
 
 greet('Hello')('Jonas');
+
+//////////////////////////////////////////////////
+// functions accepting callback function
+
+const oneWord = function (str) {
+    return str.replace(/ /g, '').toLowerCase();
+};
+
+const upperFirstWord = function (str) {
+    const [first, ...others] = str.split(' ');
+    return [first.toUpperCase(), ...others].join(' ');
+};
+
+// Higher-order function
+const transformer = function (str, fn) {
+    console.log(`Original string: ${str}`);
+    console.log(`Transformed string: ${fn(str)}`);
+
+    console.log(`Transformed by: ${fn.name}`);
+};
+
+transformer('JavaScript is the best!', upperFirstWord);
+transformer('JavaScript is the best!', oneWord);
+
+
+/////////////////////////////////////////////
+// arguments passed as values and reference
+
+// How Passing Arguments Works: Values vs. Reference
+const flight = 'LH234';
+const jonas = {
+    name: 'Jonas Schmedtmann',
+    passport: 24739479284,
+};
+
+// flightNum is just a copy of the flight variable
+// passenger object is passed as a reference (same address)
+const checkIn = function (flightNum, passenger) {
+    flightNum = 'LH999';
+    passenger.name = 'Mr. ' + passenger.name;
+
+    if (passenger.passport === 24739479284) {
+        alert('Checked in');
+    } else {
+        alert('Wrong passport!');
+    }
+};
+
+// checkIn(flight, jonas);
+// console.log(flight);
+// console.log(jonas);
+
+// Is the same as doing...
+// const flightNum = flight; //copyied the value (pass by value) 
+// const passenger = jonas; //reference type (we are only passing its reference not pass by reference)
+
+const newPassport = function (person) {
+    person.passport = Math.trunc(Math.random() * 100000000000);
+};
+
+// newPassport(jonas);
+// checkIn(flight, jonas);
+
+// NOTE::->>javascript does not have pass by reference (but it looks like it) it has only pass by value
