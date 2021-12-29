f29bd4f0cb410c7c3745517e97f9d0723c7c7d51
Akshit kumar <akshitkumar450@gmail.com>
2021-12-29 23:55:22 +0530

    logical assignment operator

--- modern-operators.js
+++ modern-operators.js
@@ -262,8 +262,41 @@ restaurant.numGuests = 0;
 //falsy value and we go to second parameter, but that is incorrect bcz 
 // numguests is equal to 0(zero) so want guests as 0
 const guests = restaurant.numGuests || 10;
-console.log(guests);
+// console.log(guests);
 
 // Nullish: null and undefined only not falsy values (NOT 0 or '')
 const guestCorrect = restaurant.numGuests ?? 10;
-console.log(guestCorrect);
+// console.log(guestCorrect);
+
+///////////////////////////////////////////////////
+// logical Assignment Operators
+const rest1 = {
+    name: 'Capri',
+    // numGuests: 20,
+    numGuests: 0,
+};
+
+const rest2 = {
+    name: 'La Piazza',
+    owner: 'Giovanni Rossi',
+};
+
+// rest1.numGuests = rest1.numGuests || 10;
+// rest2.numGuests = rest2.numGuests || 10;
+// rest1.numGuests ||= 10;
+// rest2.numGuests ||= 10;
+
+
+// nullish assignment operator (null or undefined)
+// rest1.numGuests ??= 10;
+// rest2.numGuests ??= 10;
+
+
+// AND assignment operator
+// rest1.owner = rest1.owner && '<ANONYMOUS>';
+// rest2.owner = rest2.owner && '<ANONYMOUS>';
+// rest1.owner &&= '<ANONYMOUS>';
+// rest2.owner &&= '<ANONYMOUS>';
+
+console.log(rest1);
+console.log(rest2);
