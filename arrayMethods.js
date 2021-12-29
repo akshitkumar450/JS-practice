fed420cadfb31209354d003faf9bba56f389eaec
Akshit kumar <akshitkumar450@gmail.com>
2021-12-29 12:17:48 +0530

    map method and started bankist

--- arrayMethods.js
+++ arrayMethods.js
@@ -54,7 +54,7 @@
 
 ///////////////////////////////////////
 // Looping Arrays: forEach
-const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
+// const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
 
 // movements.entires()
 // for (const mov of movements.entries()) {
@@ -91,3 +91,29 @@ Array[7, 1300]
 // 1: function(450)
 // 2: function(400)
 // ...
+
+
+///////////////////////////////////////
+// The map Method
+const eurToUsd = 1.1;
+
+// const movementsUSD = movements.map(function (mov) {
+//   return mov * eurToUsd;
+// });
+
+// const movementsUSD = movements.map(mov => mov * eurToUsd);
+
+// console.log(movements);
+// console.log(movementsUSD);
+
+// const movementsUSDfor = [];
+// for (const mov of movements) movementsUSDfor.push(mov * eurToUsd);
+// console.log(movementsUSDfor);
+
+// const movementsDescriptions = movements.map(
+//     (mov, i) =>
+//         `Movement ${i + 1}: You ${mov > 0 ? 'deposited' : 'withdrew'} ${Math.abs(
+//             mov
+//         )}`
+// );
+// console.log(movementsDescriptions);
