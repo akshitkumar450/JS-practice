a04f91d57a75f5be0294503ae36ba01b3557a754
Akshit kumar <akshitkumar450@gmail.com>
2021-12-31 17:52:06 +0530

    classes

--- OOP.js
+++ OOP.js
@@ -90,3 +90,43 @@ Person.prototype.calcAge = function () {
 
 // const h1 = document.querySelector('h1');
 // console.dir(x => x + 1);
+
+
+///////////////////////////////////////
+// ES6 Classes
+
+// Class expression
+// const PersonCl = class {}
+
+// Class declaration
+class PersonCl {
+    constructor(fullName, birthYear) {
+        this.fullName = fullName;
+        this.birthYear = birthYear;
+    }
+
+    // Instance methods
+    // Methods will be added to .prototype property
+    calcAge() {
+        console.log(2037 - this.birthYear);
+    }
+
+    greet() {
+        console.log(`Hey ${this.fullName}`);
+    }
+}
+
+const jessica = new PersonCl('Jessica Davis', 1996);
+console.log(jessica);
+jessica.calcAge();
+
+console.log(jessica.__proto__ === PersonCl.prototype);
+
+// PersonCl.prototype.greet = function () {
+//   console.log(`Hey ${this.firstName}`);
+// };
+jessica.greet();
+
+  // 1. Classes are NOT hoisted
+  // 2. Classes are first-class citizens
+  // 3. Classes are executed in strict mode
