ea87b940aa2f78be964ef04b641fcb3b92cc4a2f
Akshit kumar <akshitkumar450@gmail.com>
2021-12-31 12:16:40 +0530

    scope chain

--- this-keyword.js
+++ this-keyword.js
@@ -175,3 +175,46 @@ addExpr(2, 5, 8, 12);
 // console.log(x === window.x);
 // console.log(y === window.y);
 // console.log(z === window.z);
+
+
+///////////////////////////////////////////////
+//Scope Chain
+
+// functions are also block scoped in ES6 in strict mode
+
+function calcAge1(birthYear) {
+    const age = 2037 - birthYear;
+
+    function printAge() {
+        let output = `${firstName1}, you are ${age}, born in ${birthYear}`;
+        console.log(output);
+
+        if (birthYear >= 1981 && birthYear <= 1996) {
+            var millenial = true;
+            // Creating NEW variable with same name as outer scope's variable
+            const firstName1 = 'Steven';
+
+            // Reasssigning outer scope's variable
+            output = 'NEW OUTPUT!';
+
+            const str = `Oh, and you're a millenial, ${firstName1}`;
+            console.log(str);
+
+            function add(a, b) {
+                return a + b;
+            }
+        }
+        // console.log(str);
+        console.log(millenial);
+        // console.log(add(2, 3));
+        console.log(output);
+    }
+    printAge();
+
+    return age;
+}
+
+const firstName1 = 'Jonas';
+calcAge1(1991);
+// console.log(age);
+// printAge();
