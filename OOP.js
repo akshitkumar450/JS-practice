05377ef4efbabea026250776e794cb7109d6847a
Akshit kumar <akshitkumar450@gmail.com>
2021-12-31 16:48:35 +0530

    prototypal inheritance

--- OOP.js
+++ OOP.js
@@ -65,3 +65,28 @@ Person.prototype.calcAge = function () {
 // console.log(jonas.hasOwnProperty('firstName')); //true
 // console.log(jonas.hasOwnProperty('species'));
 // false
+
+
+///////////////////////////////////////
+// Prototypal Inheritance on Built-In Objects
+// console.log(jonas.__proto__);
+// Object.prototype (top of prototype chain)
+// console.log(jonas.__proto__.__proto__);
+// console.log(jonas.__proto__.__proto__.__proto__); //null
+
+// console.dir(Person.prototype.constructor); //Person constructor function
+
+// const arr = [3, 6, 6, 5, 6, 9, 9]; // new Array === []
+// console.log(arr.__proto__);
+// console.log(arr.__proto__ === Array.prototype);
+
+// console.log(arr.__proto__.__proto__);
+
+// Array.prototype.unique = function () {
+//     return [...new Set(this)];
+// };
+
+// console.log(arr.unique());
+
+// const h1 = document.querySelector('h1');
+// console.dir(x => x + 1);
